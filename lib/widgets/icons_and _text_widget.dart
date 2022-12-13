d5950a4efb133ab7458be0f69700424fe42d6948
Resolute07 <utkarsh070202@gmail.com>
2022-12-13 12:11:24 -0500

    in progress

---
+++ lib/widgets/icons_and _text_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/smalltext.dart';
+
+class IconsAndTextWidget extends StatelessWidget {
+  final IconData icon ;
+  final String text;
+
+  final Color iconColor;
+
+  const IconsAndTextWidget({Key? key,
+     required this.text,
+     required this.icon,
+    required this.iconColor,
+
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon,color: iconColor,),
+        SizedBox(width: 5,),
+        SmallText(text: text )
+      ],
+
+    );
+  }
+}
